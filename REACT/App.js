56433e69bbea79bddf4ad091094287ad2f567606
diksha0602 <132264579+diksha0602@users.noreply.github.com>
2023-10-01 00:31:47 +0530

    Update App.js

--- REACT/App.js
+++ REACT/App.js
@@ -1,1 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+import FunctionalComponent from "./components/FunctionalComponent";
+import ClassComp from "./components/ClassComp";
 
+function App() {
+  return (
+    <div className="App">
+      <FunctionalComponent
+        name="Diksha"
+        age="20"
+        occupation="student"
+      ></FunctionalComponent>
+      <ClassComp></ClassComp>
+    </div>
+  );
+}
+
+export default App;
