0da25e2b5853ca269199d6265291a96460728433
diksha0602 <132264579+diksha0602@users.noreply.github.com>
2023-10-01 00:28:43 +0530

    Update FunctionalComponent.js

--- REACT/components/FunctionalComponent.js
+++ REACT/components/FunctionalComponent.js
@@ -1,1 +1,17 @@
+import React from "react";
+
+function FunctionalComponent(prop_obj) {
+  console.log(prop_obj);
+  return (
+    <div>
+      <h1> I am functional Component.</h1>
+      <p>I am a paragraph inside a functional component</p>
+      <h2>{prop_obj.name}</h2>
+      <h2>{prop_obj.age}</h2>
+      <h2>{prop_obj.occupation}</h2>
+    </div>
+  );
+}
+
+export default FunctionalComponent;
 
